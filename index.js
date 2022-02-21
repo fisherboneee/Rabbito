61fbbfc8d0cfd60c14b081a50f18cdb81e26a6ea
fisherbone <yourtrulyfisherbone@gmail.com>
2022-02-22 00:11:41 +0800

    Code Formatting implemented

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
 client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+const commandFiles = fs
+    .readdirSync('./commands')
+    .filter((file) => file.endsWith('.js'));
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-    
+
     client.commands.set(command.data.name, command);
 }
 
@@ -17,7 +19,7 @@ client.once('ready', () => {
     console.log('Rabbito is ready for action!');
 });
 
-client.on('interactionCreate', async interaction => {
+client.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand()) return;
 
     const command = client.commands.get(interaction.commandName);
@@ -28,7 +30,10 @@ client.on('interactionCreate', async interaction => {
         await command.execute(interaction);
     } catch (error) {
         console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command.', ephemeral: true});
+        await interaction.reply({
+            content: 'There was an error while executing this command.',
+            ephemeral: true,
+        });
     }
 });
 
